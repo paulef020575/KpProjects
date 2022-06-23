bdb8480001795a5454bfdf863d5743181e49b279
PaulEf <paulef@mail.ru>
2022-06-23 17:58:29 +0300

    добавление клиента

---
+++ KpProjects.Connector/KpProjectsApiClient.cs
@@ -0,0 +1,46 @@
+﻿using KpProjects.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace KpProjects.Connector
+{
+    public class KpProjectsApiClient
+    {
+        private HttpClient _httpClient;
+
+        public KpProjectsApiClient(string baseAddress)
+        {
+            _httpClient = new HttpClient();
+            //_httpClient.BaseAddress = new Uri(baseAddress);
+
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<IEnumerable<TDataClass>> LoadList<TDataClass>()
+            where TDataClass : DataClass
+        {
+            TDataClass[] items = new TDataClass[0];
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(@"http://localhost:42216/Person");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    items = await response.Content.ReadAsAsync<TDataClass[]>();
+                }
+
+                return items?.ToList();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ KpProjects.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using KpProjects.Classes;
+using KpProjects.Connector;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KpProjects.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            KpProjectsApiClient client = new KpProjectsApiClient(@"https://localhost:44363/");
+            listBox.ItemsSource = await client.LoadList<Person>();
+        }
+    }
+}
